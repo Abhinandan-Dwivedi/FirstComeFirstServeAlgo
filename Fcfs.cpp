4ee9695b6587fbbbadf8afbc2650323ea1e83c97
Abhinandan <dwabhi2006@gmail.com>
2025-05-03 18:46:45 +0530

    add The

--- Fcfs.cpp
+++ Fcfs.cpp
@@ -8,7 +8,7 @@ int main()
     int n;
     cout << "Enter the number of process" << endl;
     cin >> n;
-    cout << "Enter Arrival time " << endl;
+    cout << "Enter The Arrival time " << endl;
     vector<int> at;
     for (int i = 0; i < n; i++)
     {
